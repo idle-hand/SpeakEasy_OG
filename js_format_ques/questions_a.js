0402b40b0ccdd0341e997f29aa58d5d7da484444
David Howe <33843929+idle-hand@users.noreply.github.com>
2023-02-14 13:41:18 -0500

    Add files via upload

---
+++ js_format_ques/questions_a.js
@@ -0,0 +1,48 @@
+{
+text: "What country is Canada’s largest trading partner?",
+options: ["Mexico", "United States of America", "China", "Japan"],
+answer: 1
+},
+{
+text: "What did the Canadian Pacific Railway symbolize?",
+options: ["Easy access to the West Coast", "What can be achieved by working together", "Unity", "Ribbons of steel"],
+answer: 2
+},
+{
+text: "What did the Fathers of Confederation do?",
+options: ["They worked together to establish a new country, the Dominion of Canada", "They were explorers who formed an expedition to survey Northern Canada", "They formed a republic state in Canada", "They were a group of politicians that tried to unite Canada to the United States"],
+answer: 0
+},
+{
+text: "What do you call a law before it is passed?",
+options: ["A New law", "A Proposed law", "A Bill", "A New proposal"],
+answer: 2
+},
+{
+text: "What do you call the Sovereign’s representative in the provinces?",
+options: ["Premier", "Member of the Legislative Assembly", "Lieutenant-Governor", "Senator"],
+answer: 2
+},
+{
+text: "What do you mark on a federal election ballot?",
+options: ["The candidate's name", "The number for the candidate", "An 'X'", "The voter's name"],
+answer: 2
+},
+{
+text: "What does Confederation mean?",
+options: ["The United States Confederate soldiers came to Canada", "Joining of communities to become a province", "Joining of suburbs to form a large city", "Joining of provinces to make a new country"],
+answer: 3
+},
+{
+text: "What does it mean to say Canada is a constitutional monarchy?",
+options: ["The Sovereign (Queen or King) is the law maker of Canada", "Canada's Head of State is a hereditary Sovereign (Queen or King) who reigns in accordance with the Constitution", "The Sovereign (Queen or King) represents Canadians in Parliament", "The Sovereign (Queen or King) approves bills before becoming law"],
+answer: 1
+},
+{
+text: "What does the “right to a secret ballot” mean?",
+options: ["No one should tell you where to vote", "A secret vote used by politicians when they are voting on sensitive topics", "No one can watch your vote and no one should look at how you voted", "Your right to vote in secret on who to appoint to the Senate"],
+answer: 2
+}
+
+
+
---
+++ js_format_ques/questions_b.js
@@ -0,0 +1,54 @@
+  {    text: "A Member of Parliament from Montreal announces that she will spend her weekend in her electoral district. This means she would be:",    options: ["In her office on Parliament Hill.", "Visiting the province of Quebec.", "In the part of Montreal where she was elected.", "Going on a vacation."],
+    answer: 2
+  },
+  {
+    text: "After a federal election, which party forms the new government?",
+    options: ["The party with the most elected representatives is invited by the Governor General to become the party in power.", "The Queen herself picks any party to run the government.", "The Governor General proposes a law for elected officials to become the governing body.", "The Premiers of each province pick a party to run the government"],
+    answer: 0
+  },
+  {
+    text: "Approximately how many Canadians served in the First World War?",
+    options: ["7000.", "8 million.", "About 60,000.", "More than 600,000."],
+    answer: 3
+  },
+  {
+    text: "Canada has three territories and how many provinces?",
+    options: ["13", "10", "3", "5"],
+    answer: 1
+  },
+  {
+    text: "Fatima is a new immigrant in Canada. What law allows her to take a job at par with a man?",
+    options: ["Equality of women and men", "Equality of all races.", "Equal pay for equal work.", "Equal rights."],
+    answer: 0
+  },
+  {
+    text: "From where does the name “Canada” come from?",
+    options: ["From the Inuit word meaning country.", "From the French word meaning joining.", "From the Métis word meaning rivers.", "From “Kanata”, the Huron-Iroquois word for village."],
+    answer: 3
+  },
+  {
+    text: "Give an example of how you can show responsibility by participating in your community.",
+    options: ["Mind your own business.", "Have a party.", "Keep your property tidy.", "Volunteer."],
+    answer: 3
+  },
+  {
+    text: "Give an example of where English and French have equal status in Canada.",
+    options: ["In schools.", "In the workplace.", "In the Parliament of Canada.", "At City Hall."],
+    answer: 2
+  },
+  {
+    text: "Give the first line of Canada’s national anthem?",
+    options: ["O Canada! Our home and native land!", "O Canada! Our province and native land!", "O Canada! From far and wide, O Canada,", "O Canada! We stand on guard for thee."],
+    answer: 0
+  },
+  {
+    text: "How are Members of Parliament chosen?",
+    options: ["Appointed by the Prime Minister.", "Elected by Canadian citizens.", "Appointed by the Queen.", "Elected by the Provincial Ministers."],
+    answer: 1
+  },
+  {
+    text: "How are Senators chosen?",
+    options: ["By the Governor General of Canada.", "By the Premiers of all provinces.", "Appointed by the Queen.", "Senators are appointed by the Governor General on the advice of the Prime Minister."],
+    answer: 3
+  },
+ 
